ea7e793e79069ec0f3a773a92445c0e6a4fc7bf9
cej4126 <42898696+cej4126@users.noreply.github.com>
2021-05-11 20:09:45 -0500

    Added X11OnX12 for imgui

--- MipTest/Graphics.cpp
+++ MipTest/Graphics.cpp
@@ -15,6 +15,8 @@ Graphics::Graphics(HWND hWnd, int width, int height)
 #endif
 
    loadDevice();
+   loadBase();
+   loadX11OnX12Base();
 }
 
 Graphics::~Graphics()
@@ -99,4 +101,136 @@ void Graphics::loadDevice()
 
    ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(m_comandQueue.ReleaseAndGetAddressOf())));
 
+   // Create Swap Chains
+   DXGI_SWAP_CHAIN_DESC1 swapChainDesc;
+   ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
+   swapChainDesc.Width = m_width;
+   swapChainDesc.Height = m_height;
+   swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+   swapChainDesc.Stereo = FALSE;
+   swapChainDesc.SampleDesc.Count = 1;
+   swapChainDesc.SampleDesc.Quality = 0;
+   swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+   swapChainDesc.BufferCount = m_bufferCount;
+   swapChainDesc.Scaling = DXGI_SCALING_NONE;
+   swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+   swapChainDesc.Flags = 0;
+   ThrowIfFailed(m_dxgiFactory4->CreateSwapChainForHwnd(m_comandQueue.Get(),
+      m_hWnd,
+      &swapChainDesc,
+      nullptr,
+      nullptr,
+      m_swapChain1.ReleaseAndGetAddressOf()));
+
+   ThrowIfFailed(m_swapChain1.As(&m_swapChain));
+
+   // Get Swap Chain Buffers
+   for (UINT i = 0; i < m_bufferCount; i++)
+   {
+      ThrowIfFailed(m_swapChain->GetBuffer(i, IID_PPV_ARGS(m_swapChainBuffer[i].ReleaseAndGetAddressOf())));
+   }
+   m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+}
+
+void Graphics::loadBase()
+{
+   // Create Descriptor Heap Render Target View (RTV)
+   D3D12_DESCRIPTOR_HEAP_DESC heapDescriptor;
+   ZeroMemory(&heapDescriptor, sizeof(heapDescriptor));
+   heapDescriptor.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+   heapDescriptor.NodeMask = 0;
+   heapDescriptor.NumDescriptors = m_bufferCount;
+   heapDescriptor.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+
+   ThrowIfFailed(m_device->CreateDescriptorHeap(&heapDescriptor, IID_PPV_ARGS(m_rtvHeap.ReleaseAndGetAddressOf())));
+
+   m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+   for (UINT i = 0; i < m_bufferCount; i++)
+   {
+      D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = m_rtvHeap->GetCPUDescriptorHandleForHeapStart();
+      rtvHandle.ptr += (UINT64)i * (UINT64)m_rtvDescriptorSize;
+      m_device->CreateRenderTargetView(m_swapChainBuffer[i].Get(), nullptr, rtvHandle);
+   }
+
+   // Create Command Allocators
+   for (UINT i = 0; i < m_bufferCount; i++)
+   {
+      ComPtr<ID3D12CommandAllocator> commandAllocator;
+      ThrowIfFailed(m_device->CreateCommandAllocator(
+         D3D12_COMMAND_LIST_TYPE_DIRECT,
+         IID_PPV_ARGS(commandAllocator.ReleaseAndGetAddressOf())));
+      m_commandAllocators.push_back(commandAllocator);
+   }
+
+   // Create Command List
+   ThrowIfFailed(m_device->CreateCommandList(
+      0,
+      D3D12_COMMAND_LIST_TYPE_DIRECT,
+      m_commandAllocators[0].Get(),
+      nullptr, IID_PPV_ARGS(m_commandList.ReleaseAndGetAddressOf())));
+
+   CreateFence();
+}
+
+void Graphics::CreateFence()
+{
+   // Create Fence
+   for (UINT i = 0; i < m_bufferCount; i++)
+   {
+      ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(m_fence[i].ReleaseAndGetAddressOf())));
+      m_fenceValue[i] = 0;
+   }
+
+   // Create Fence Event Handle
+   m_fenceHandle = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+   if (m_fenceHandle == NULL)
+   {
+      throw;
+   }
+}
+
+void Graphics::loadX11OnX12Base()
+{
+   UINT x11DeviceFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+#if defined(_DEBUG)
+   x11DeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+   ThrowIfFailed(D3D11On12CreateDevice(m_device.Get(),
+      x11DeviceFlags,
+      nullptr, 0,
+      reinterpret_cast<IUnknown **>(m_comandQueue.GetAddressOf()),
+      1, 0, &m_x11Device, &m_x11Context, nullptr));
+
+   // Query the X11OnX12 device from the X11 device
+   ThrowIfFailed(m_x11Device.As(&m_x11Onx12Device));
+
+   // Create RTV
+   for (UINT i = 0; i < m_bufferCount; i++)
+   {
+      D3D11_RESOURCE_FLAGS d3d11Flags = { D3D11_BIND_RENDER_TARGET };
+      ThrowIfFailed(m_x11Onx12Device->CreateWrappedResource(
+         m_swapChainBuffer[i].Get(),
+         &d3d11Flags,
+         D3D12_RESOURCE_STATE_RENDER_TARGET,
+         D3D12_RESOURCE_STATE_PRESENT,
+         IID_PPV_ARGS(&x11WrappedBackBuffer[i])));
+
+      ThrowIfFailed(m_x11Device->CreateRenderTargetView(x11WrappedBackBuffer[i].Get(), nullptr, &m_x11Target[i]));
+   }
+
+   m_x11ViewPort.Width = (float)m_width;
+   m_x11ViewPort.Height = (float)m_height;
+   m_x11ViewPort.MaxDepth = 1;
+   m_x11ViewPort.MinDepth = 0;
+   m_x11ViewPort.TopLeftX = 0;
+   m_x11ViewPort.TopLeftY = 0;
+
+   // Depth Stencil
+   D3D11_DEPTH_STENCIL_DESC depthDescription = {};
+   depthDescription.StencilEnable = false;
+   depthDescription.DepthEnable = false;
+
+   ThrowIfFailed(m_x11Device->CreateDepthStencilState(
+      &depthDescription, &m_x11DepthStencelState));
 }
--- MipTest/Graphics.h
+++ MipTest/Graphics.h
@@ -33,16 +33,41 @@ public:
    void setMatrixConstant(UINT index, TransformMatrix matrix, int rootVS, int rootPS) noexcept;
 
 protected:
+   static const UINT m_bufferCount = 3;
    HWND m_hWnd;
    int m_width;
    int m_height;
+   UINT m_frameIndex;
+   int m_rtvDescriptorSize;
+   UINT64 m_fenceValue[m_bufferCount];
+   HANDLE m_fenceHandle;
 
    ComPtr <IDXGIFactory4> m_dxgiFactory4;
    ComPtr <ID3D12Device> m_device;
    ComPtr <ID3D12CommandQueue>m_comandQueue;
+   ComPtr <IDXGISwapChain3> m_swapChain;
+   ComPtr <IDXGISwapChain1> m_swapChain1;
+   ComPtr <ID3D12Resource> m_swapChainBuffer[m_bufferCount];
+   ComPtr <ID3D12DescriptorHeap> m_rtvHeap;
+   std::vector<ComPtr <ID3D12CommandAllocator>> m_commandAllocators;
+   ComPtr <ID3D12CommandList> m_commandList;
+   ComPtr <ID3D12Fence> m_fence[m_bufferCount];
+
+   // Direct X11
+   ComPtr <ID3D11Device> m_x11Device;
+   ComPtr <ID3D11DeviceContext> m_x11Context;
+   ComPtr <ID3D11On12Device> m_x11Onx12Device;
+   ComPtr <ID3D11Resource> x11WrappedBackBuffer[m_bufferCount];
+   ComPtr <ID3D11RenderTargetView> m_x11Target[m_bufferCount];
+   D3D11_VIEWPORT m_x11ViewPort;
+   ComPtr <ID3D11DepthStencilState> m_x11DepthStencelState;
 
 private:
    void loadDevice();
+   void loadBase();
+   void CreateFence();
+   void loadX11OnX12Base();
+
 
    float m_aspectRatio;
 };
